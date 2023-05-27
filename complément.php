13af3096bd2af9a29047232728a52bd0003f81e0
ChouaibGema <133110038+ChouaibGema@users.noreply.github.com>
2023-05-27 19:10:36 +0200

    Create complément.php

---
+++ complément.php
@@ -0,0 +1,9 @@
+// j'ai pas arriver à modifier le fichier de functionfoo.php pour ajouter l'appel et les résultats de cette fonction
+// je rajoute ça ici , merci pour votre compréhension
+
+
+var_dump(foo([[7, 11], [12, 27], [3, 8], [2, 6], [6, 18], [4, 5], [16, 17]]);
+var_dump(foo([[1, 5], [4, 5]]);
+
+// le test m'a pris à peut près 40min 
+// merci
